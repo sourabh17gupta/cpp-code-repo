537325ac7f35d5229e96d639cb47c1d914f1f894
Sourabh Gupta <123104060@nitkkr.ac.in>
2025-09-28 23:23:01 +0530

    final fix std namespace

--- ordered_set.cpp
+++ ordered_set.cpp
@@ -2,7 +2,7 @@
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-
+using namespace std;
 template<typename T>
 using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
 
